26ddb01792566556946b31280fea78ac53bf9d64
Kanok <kanoksaengjan@hotmail.com>
2024-11-29 02:34:12 +0700

    I am Groot. Just trolling lol

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -5,9 +5,8 @@ import Navbar from "../components/Navbar"
 export default function portfolio(){
     return(
     <div className="bg-[#f8f9fa]">  
-
         <div className="floating-box"></div>
-        <p>Hi</p>
+        <p>I am Groot</p>
     </div>
     )
 
