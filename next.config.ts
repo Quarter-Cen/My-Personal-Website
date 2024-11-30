8878924785601d71bffd73f71f9944c692b0bd7b
Kanok <kanoksaengjan@hotmail.com>
2024-11-30 16:26:16 +0700

    fix cache Logo

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/Logo2.png',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable', // Cache for 1 year
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
