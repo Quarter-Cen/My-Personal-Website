4fe5156d3902537d0c92c0ce0a3f3aa74e7bb992
Kanok <kanoksaengjan@hotmail.com>
2024-11-30 18:39:10 +0700

    Add skeleton about, contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,21 @@
+'use client'
+import React, { useState, useRef, useEffect } from "react";
+import Navbar from "../components/Navbar"
+
+export default function portfolio(){
+    return(
+    <>  
+        <div className="floating-box"></div>
+        <div className="fixed z-50">
+            <Navbar currentIndex={1} />
+        </div>
+        <div className="content">
+            <div className="section">
+                Under Construct
+            </div>
+        </div>
+    </>
+    )
+
+    
+}
